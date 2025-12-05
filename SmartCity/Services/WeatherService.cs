c9cc892ad38373663fc7bd8f4b5bd58b9ed6d5f3
Parveen Kaur Maan <maanparveen47@gmail.com>
2025-12-05 12:45:27 -0800

    changes made

--- SmartCity/Services/WeatherService.cs
+++ SmartCity/Services/WeatherService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -23,7 +25,7 @@ namespace SmartCity.Services
             if (string.IsNullOrWhiteSpace(city))
                 throw new ArgumentException("City must be provided", nameof(city));
 
-            var url = $"https://api.openweathermap.org/data/2.5/weather?q={Uri.EscapeDataString(city)}&units=metric&appid={_apiKey}";
+            var url = $"https://api.openweathermap.org/data/2.5/forecast?q={Uri.EscapeDataString(city)}&units=metric&cnt=40&appid={_apiKey}";
             using var res = await _http.GetAsync(url);
 
             if (!res.IsSuccessStatusCode)
@@ -36,17 +38,56 @@ namespace SmartCity.Services
             using var doc = await JsonDocument.ParseAsync(stream);
             var root = doc.RootElement;
 
-            var temp = root.GetProperty("main").GetProperty("temp").GetDouble();
-            var weatherArray = root.GetProperty("weather");
-            var condition = weatherArray[0].GetProperty("main").GetString() ?? "Unknown";
-            var icon = weatherArray[0].GetProperty("icon").GetString();
+            var city_data = root.GetProperty("city");
+            var cityName = city_data.GetProperty("name").GetString() ?? city;
+
+            var list = root.GetProperty("list");
+            var forecasts = new List<ForecastItem>();
+
+            var processedDates = new HashSet<string>();
+
+            foreach (var item in list.EnumerateArray())
+            {
+                var dt_txt = item.GetProperty("dt_txt").GetString();
+                var dateOnly = dt_txt?.Substring(0, 10);
+
+                if (processedDates.Contains(dateOnly))
+                    continue;
+
+                processedDates.Add(dateOnly);
+
+                if (processedDates.Count > 3)
+                    break;
+
+                var dt = DateTime.ParseExact(dt_txt, "yyyy-MM-dd HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+                var mainData = item.GetProperty("main");
+                var weatherArray = item.GetProperty("weather");
+
+                var forecastItem = new ForecastItem
+                {
+                    Date = dt,
+                    Min = mainData.GetProperty("temp_min").GetDouble(),
+                    Max = mainData.GetProperty("temp_max").GetDouble(),
+                    Condition = weatherArray[0].GetProperty("main").GetString() ?? "Unknown"
+                };
+
+                forecasts.Add(forecastItem);
+            }
+
+            var currentData = list[0];
+            var currentMain = currentData.GetProperty("main");
+            var currentWeather = currentData.GetProperty("weather");
+            var currentTemp = currentMain.GetProperty("temp").GetDouble();
+            var currentCondition = currentWeather[0].GetProperty("main").GetString() ?? "Unknown";
+            var currentIcon = currentWeather[0].GetProperty("icon").GetString();
 
             return new WeatherModel
             {
-                City = root.GetProperty("name").GetString() ?? city,
-                TempC = temp,
-                Condition = condition,
-                Icon = icon,
+                City = cityName,
+                TempC = currentTemp,
+                Condition = currentCondition,
+                Icon = currentIcon,
+                Forecast = forecasts,
                 FetchedAt = DateTime.UtcNow
             };
         }
